480738b8defbd82a2ebc6aa156bfa3ffb5b5dfc4
c87 <shaun.carmody@live.co.uk>
2019-03-18 20:51:37 +0000

    get, post, update and delete useres

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // implement your API here
 const express = require('express');
-const db = require('./data/dj.js');
+const db = require('./data/db.js');
 const server = express();
 server.use(express.json());
 
@@ -11,14 +11,14 @@ server.post('/api/users', (req, res) => {
 
   if (!user.name || !user.bio) {
     res
-      .json(400)
-      .end({ errorMessage: "Please provide name and bio for the user." });
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user." });
   }
 
   db.insert(user)
     .then(user => {
       res
-        .status(200)
+        .status(201)
         .json(user)
     })
     .catch(err => {
@@ -49,17 +49,23 @@ server.get('/api/users', (req, res) => {
 // GET	/api/users/:id
 
 server.get('/api/users/:id', (req, res) => {
-  const { id } = req.body;
+  const { id } = req.params;
   db.findById(id)
     .then(user => {
-      res
-        .status(200)
-        .json(user)
+      if (!user) {
+        res
+          .status(404)
+          .json({ message: "The user with the specified ID does not exist." });
+      } else {
+        res
+          .status(200)
+          .json(user)
+      }
     })
     .catch(err => {
       res
-        .status(404)
-        .json({ message: "The user with the specified ID does not exist." });
+        .status(500)
+        .json({ error: "The user information could not be retrieved." })
     });
 });
 
@@ -68,7 +74,7 @@ server.get('/api/users/:id', (req, res) => {
 
 
 server.delete('/api/users/:id', (req, res) => {
-  const { id } = req.body;
+  const { id } = req.params;
   db.remove(id)
     .then(user => {
       if (!user) {
@@ -78,7 +84,7 @@ server.delete('/api/users/:id', (req, res) => {
       } else {
         res
           .status(200)
-          .json(user.length)
+          .json(user)
       }
     })
     .catch(err => {
@@ -92,38 +98,34 @@ server.delete('/api/users/:id', (req, res) => {
 // PUT	/api/users/:id
 
 server.put('/api/users/:id', (req, res) => {
-  const { id } = req.body;
+  const { id } = req.params;
   const user = req.body;
 
-  if (!user.name || !user.bio) {
+ if (!user.name || !user.bio) {
     res
       .status(400)
-      .end({ errorMessage: "Please provide name and bio for the user." });
+      .json({ errorMessage: "Please provide name and bio for the user." });
   }
 
-  db.findById(id)
+  db.update(id, user)
     .then(user => {
       if (!user) {
         res
           .status(404)
-          .json({ message: "The user with the specified ID does not exist." });
+          .json({ message: "The user with the specified ID does not exist." })
       } else {
-        db.update(id, user)
-          .then(updatedUser => {
-            res
-              .status(200)
-              .json(updatedUser)
-          })
-          .catch(err => {
-            res
-              .status(500)
-              .json({ error: "The user information could not be modified." });
-          })
+        res
+          .status(200)
+          .json(user)
       }
     })
     .catch(err => {
       res
         .status(500)
-        .json({ error: "The user information could be found (catch)." });
+        .json({ error: "The user information could not be modified." });
     });
+});
+
+server.listen(5000, () => {
+  console.log('Server listening on Port 5000');
 });
